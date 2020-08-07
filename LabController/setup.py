2dbc504dc6ffbbbcb950346d20ae3f78513c3e3f
Martin Styk <mastyk@redhat.com>
2020-08-07 17:34:28 +0200

    lab controller: drop init.d files
    Signed-off-by: Martin Styk <mastyk@redhat.com>

--- LabController/setup.py
+++ LabController/setup.py
@@ -2,18 +2,21 @@ from setuptools import setup, find_packages
 import commands
 from glob import glob
 
+
 def systemd_unit_dir():
     status, output = commands.getstatusoutput('pkg-config --variable systemdsystemunitdir systemd')
     if status or not output:
-        return None # systemd not found
+        return None  # systemd not found
     return output.strip()
+
 
 def systemd_tmpfiles_dir():
     # There doesn't seem to be a specific pkg-config variable for this
     status, output = commands.getstatusoutput('pkg-config --variable prefix systemd')
     if status or not output:
-        return None # systemd not found
+        return None  # systemd not found
     return output.strip() + '/lib/tmpfiles.d'
+
 
 data_files = [
     ('/etc/beaker/', ['labcontroller.conf']),
@@ -29,23 +32,14 @@ data_files = [
     ('/var/www/beaker/logs', []),
     ('/usr/share/bkr/lab-controller', ['apache/404.html'] + glob('aux/*')),
 ]
-if systemd_unit_dir():
-    data_files.extend([
-        (systemd_unit_dir(), ['systemd/beaker-proxy.service',
-                              'systemd/beaker-provision.service',
-                              'systemd/beaker-watchdog.service',
-                              'systemd/beaker-transfer.service']),
-        (systemd_tmpfiles_dir(), ['tmpfiles.d/beaker-lab-controller.conf']),
-        ('/run/beaker-lab-controller', []),
-    ])
-else:
-    data_files.extend([
-        ('/etc/init.d', ['init.d/beaker-proxy',
-                         'init.d/beaker-transfer',
-                         'init.d/beaker-provision',
-                         'init.d/beaker-watchdog']),
-        ('/var/run/beaker-lab-controller', []),
-    ])
+data_files.extend([
+    (systemd_unit_dir(), ['systemd/beaker-proxy.service',
+                          'systemd/beaker-provision.service',
+                          'systemd/beaker-watchdog.service',
+                          'systemd/beaker-transfer.service']),
+    (systemd_tmpfiles_dir(), ['tmpfiles.d/beaker-lab-controller.conf']),
+    ('/run/beaker-lab-controller', []),
+])
 
 setup(
     name='beaker-lab-controller',
@@ -60,12 +54,12 @@ setup(
     ],
 
     packages=find_packages('src'),
-    package_dir = {'':'src'},
+    package_dir={'': 'src'},
 
-    namespace_packages = ['bkr'],
+    namespace_packages=['bkr'],
 
     data_files=data_files,
-    package_data = {
+    package_data={
         'bkr.labcontroller': [
             'default.conf',
             'power-scripts/*',
@@ -74,14 +68,14 @@ setup(
         ],
     },
 
-    classifiers = [
+    classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Operating System :: POSIX :: Linux',
         'Programming Language :: Python :: 2.7',
         'License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)',
     ],
 
-    entry_points = {
+    entry_points={
         'console_scripts': (
             'beaker-proxy    = bkr.labcontroller.main:main',
             'beaker-watchdog = bkr.labcontroller.watchdog:main',

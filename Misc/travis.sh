38a7fd51a2f28942cade70a4df368ef8d6453e69
Martin Styk <mastyk@redhat.com>
2020-08-11 21:16:20 +0200

    ci: install steps
    Signed-off-by: Martin Styk <mastyk@redhat.com>

--- Misc/travis.sh
+++ Misc/travis.sh
@@ -4,14 +4,86 @@ set -eux
 
 method=$1
 
-function travis_before_install
+function travis_before_install()
 {
 	echo "Verify that virtualization is available ..."
-	kvm-ok || true
-	grep 'vmx\|kvm' /proc/cpuinfo || true
-	ls -l /dev/kvm || true
-	grep -r kvm /lib/udev/rules.d
+	kvm-ok
 	echo "Verify that virtualization is available ...  done"
+}
+
+function travis_install()
+{
+  REVISION="2003"
+  IMAGE_BASE_NAME="CentOS-7-x86_64-GenericCloud-$REVISION.raw"
+  TAR_BASE_NAME="$IMAGE_BASE_NAME.tar.gz"
+  IMAGE_URL="https://cloud.centos.org/centos/7/images/$TAR_BASE_NAME"
+  SUM_URL="https://cloud.centos.org/centos/7/images/sha256sum.txt"
+  MEMORY=4096
+  VCPUS="$(nproc)"
+  sudo usermod -a -G kvm,libvirt,libvirt-qemu "$USER"
+
+  # Spin that virtualization
+  sudo systemctl enable libvirtd
+  sudo systemctl start libvirtd
+
+  # SSH Keys
+  ssh-keygen -N "" -f "$HOME/.ssh/id_rsa"
+
+  cd "$HOME"
+  wget "$IMAGE_URL"
+  wget "$SUM_URL"
+
+  # Verify
+  sha256sum --ignore-missing -c ./sha256sum.txt
+
+  # Unpack
+  tar -xvf $TAR_BASE_NAME
+
+  # Search is needed for $HOME so virt service can access the image file.
+  chmod a+x "$HOME"
+
+  sudo virt-sysprep -a "$IMAGE_BASE_NAME" \
+  --root-password password:123456 \
+  --hostname centosvm \
+  --mkdir /root/.ssh \
+  --upload "$HOME/.ssh/id_rsa.pub:/root/.ssh/authorized_keys" \
+  --chmod '0600:/root/.ssh/authorized_keys' \
+  --run-command 'chown root:root /root/.ssh/authorized_keys' \
+  --copy-in "$TRAVIS_BUILD_DIR:/root" \
+  --network \
+  --selinux-relabel
+
+  sudo virt-install \
+  --name centosvm \
+  --memory $MEMORY \
+  --vcpus $VCPUS \
+  --disk "$IMAGE_BASE_NAME" \
+  --os-variant rhel7 \
+  --os-type linux \
+  --import \
+  --noautoconsole
+
+  sleep 30
+    for i in $(seq 0 29); do
+    echo "loop $i"
+      sleep 6s
+      # Get the leases, but tee it so it's easier to debug
+      sudo virsh net-dhcp-leases default | tee dhcp-leases.txt
+
+      # get our ipaddress
+      ipaddy="$(grep centosvm dhcp-leases.txt | awk '{print $5}' | cut -d'/' -f 1-1)"
+      if [ -n "$ipaddy" ]; then
+          # found it, we're done looking, print it for debug logs
+          echo "ipaddy: $ipaddy"
+          break
+    fi
+    # it's empty/not found, loop back and try again.
+  done
+
+  if [ -z "$ipaddy" ]; then
+      echo "ipaddy zero length, exiting with error 1"
+      exit 1
+  fi
 }
 
 travis_"$method"
